ccbc089814e9685769301f49f2783c1e04799451
Syahreza-Ferdian <syahrezafistiferdian32@gmail.com>
2026-03-29 23:22:20 +0700

    `feat`: enhance main application setup with global pipes, filters, interceptors, and CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { HttpErrorFilter } from './common/exceptions/http/http.filter';
+import { ResponseInterceptor } from './common/response/response.interceptor';
+import * as dotenv from 'dotenv';
+import { PrismaFilter } from './common/exceptions/prisma/prisma.filter';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  app.useGlobalFilters(new HttpErrorFilter());
+  app.useGlobalFilters(new PrismaFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor(new Reflector()));
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
+  console.info(`Application is running on: ${await app.getUrl()}`);
 }
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
